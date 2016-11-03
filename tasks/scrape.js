9abeee86371e409432981fb02595c10ce9d59526
Thomas Wilburn <news@thomaswilburn.net>
2016-11-03 15:19:58 -0700

    Move zoomed districts into the election data

--- tasks/scrape.js
+++ tasks/scrape.js
@@ -191,6 +191,7 @@ module.exports = function(grunt) {
         // move Key Races to the front
         categories: ["Key Races"].concat(Object.keys(categorized).filter(r => r != "Key Races")),
         mapped: countyData.mapped,
+        zoomed: [1,11,21,23,25,26,27,28,29,30,32,33,34,36,37,38,41,43,44,45,46,47,48],
         turnout: turnout,
         updated: getDateline(),
         widget: widget
